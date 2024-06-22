6cfe29ce841d6418b2a3139958a50937c04d6b13
Juan Augusto Haser <juanaugustohaser@gmail.com>
2024-06-22 19:14:07 -0300

    Feature: Created home + tech component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TechComponent } from './tech/tech.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TechComponent
   ],
   imports: [
     BrowserModule,

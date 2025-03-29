374c2cdf4bdbba683b1468b1e99ea4bb05112adf
Juan Augusto Haser <juanaugustohaser@gmail.com>
2025-03-28 23:49:23 -0300

    Semantics corrections

--- src/app/home/section-profile/section-profile.component.ts
+++ src/app/home/section-profile/section-profile.component.ts
@@ -8,57 +8,25 @@
     styleUrls: ['./section-profile.component.css']
   })
   
-  export class SectionProfileComponent implements OnInit {
+  export class SectionProfileComponent {
     @Output() themeChanged = new EventEmitter<boolean>();
     urlTheme: string = '../../../assets/icons8-moon-90-removebg-preview.png';
     theme: boolean = false;
     urlLang: string = '../../../assets/icons8-spain-flag-48-removebg-preview.png';
     lang: boolean = false;
     clicked: boolean = false;
-    groupedIcons: { title: string, src: string, alt: string }[][] = []; // Actualiza el tipo aquí
     first: boolean = true;
     third: boolean = true;
     six: boolean = true;
     height: boolean = true;
 
     icons = [
-      { title: 'Angular', src: '../../assets/icons8-angular-96-removebg-preview.png', alt: 'Angular icon' },
-      { title: 'Html', src: '../../assets/icons8-html-96-removebg-preview.png', alt: 'Html icon' },
-      { title: 'Typescript', src: '../../assets/icons8-typescript-144-removebg-preview.png', alt: 'Typescript icon' },
-      { title: 'Python', src: '../../assets/icons8-python-144-removebg-preview.png', alt: 'Python icon' },
-      { title: 'Css', src: '../../assets/icons8-css-96-removebg-preview.png', alt: 'Css icon' },
-      { title: 'Ionic', src: '../../assets/icons8-ionic-a-complete-open-source-sdk-for-hybrid-mobile-app-development-96-removebg-preview.png', alt: 'Ionic icon' },
-      { title: 'php', src: '../../assets/icons8-php-160-removebg-preview.png', alt: 'Php icon' },
-      { title: 'Java', src: '../../assets/icons8-java-100.png', alt: 'Java icon' },
-      { title: 'Mysql', src: '../../assets/icons8-mysql-logo-144.png', alt: 'Mysql icon' },
-      { title: 'SpringBoot', src: '../../assets/icons8-spring-boot-160.png', alt: 'Springboot icon' }
+      { title: 'Github', src: '../../assets/icons8-github-50.png', alt: 'Angular icon' },
+      { title: 'Linkedin', src: '../../assets/icons8-linkedin-50.png', alt: 'Typescript icon' },
+      { title: 'Juanaugustohaser@gmail.com', src: '../../assets/icons8-email-30.png', alt: 'Html icon' }
     ];
 
     constructor(private translate: TranslateService, private bookGameService: BookGameService){}
-
-    ngOnInit() {
-      this.updateIconGrouping(window.innerWidth);
-    }
-
-    @HostListener('window:resize', ['$event'])
-    onResize(event: any) {
-      this.updateIconGrouping(event.target.innerWidth);
-    }
-
-    updateIconGrouping(screenWidth: number) {
-      if (screenWidth > 991) 
-        this.groupedIcons = this.chunkArray(this.icons, 7);
-      else {
-        if (screenWidth > 700) 
-          this.groupedIcons = this.chunkArray(this.icons, 5);
-        else{
-          if (screenWidth > 320) 
-            this.groupedIcons = this.chunkArray(this.icons, 3);
-          else{
-            this.groupedIcons = this.chunkArray(this.icons, 2);
-          }        }
-      }
-    }
 
     switchTheme() {
       this.theme = !this.theme;
